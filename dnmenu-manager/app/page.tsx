0430218e7a4eac3a925c48309d4776dd77895511
lukit00 <danielalvesgodinho05@gmail.com>
2026-01-31 01:52:36 -0300

    ui

--- dnmenu-manager/app/page.tsx
+++ dnmenu-manager/app/page.tsx
@@ -420,7 +420,7 @@ function RodaPe() {
     );
 }
 
-export default function PaginaPrincipal() {
+export default function Page() {
     return (
         <div className="min-h-screen bg-gradient-to-b from-white via-white to-gray-50">
             <Navbar />
